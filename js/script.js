afcf9a0f2a4425ec39af3982dee9ff98922f549d
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-15 17:08:18 +0100

    -visualizzazione dinamica contatti-

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const app = new Vue ({
+    el: '#app',
+    data: {
+        usersList: [
+            {
+                name: 'Michele',
+                img: 'avatar_1.jpg',
+                text: 'Ultimo messaggio inviato',
+                time: '12:00',
+            },
+            {
+                name: 'Fabio',
+                img: 'avatar_2.jpg',
+                text: 'Ultimo messaggio inviato',
+                time: '12:00',
+            },
+            {
+                name: 'Samuele',
+                img: 'avatar_3.jpg',
+                text: 'Ultimo messaggio inviato',
+                time: '12:00',
+            },
+            {
+                name: 'Alessandro B.',
+                img: 'avatar_4.jpg',
+                text: 'Ultimo messaggio inviato',
+                time: '12:00',
+            },
+            {
+                name: 'Alessandro L.',
+                img: 'avatar_5.jpg',
+                text: 'Ultimo messaggio inviato',
+                time: '12:00',
+            },
+            {
+                name: 'Claudia',
+                img: 'avatar_6.jpg',
+                text: 'Ultimo messaggio inviato',
+                time: '12:00',
+            },
+            {
+                name: 'Federico',
+                img: 'avatar_7.jpg',
+                text: 'Ultimo messaggio inviato',
+                time: '12:00',
+            },
+            {
+                name: 'Davide',
+                img: 'avatar_8.jpg',
+                text: 'Ultimo messaggio inviato',
+                time: '12:00',
+            },
+        ]
+    }
+})
